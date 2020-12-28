18776e211531ef8880e89f6c74f12a4b658083ec
Anuj Chatterjee <anujchatterjee@gmail.com>
2020-12-27 17:42:40 -0800

    port adjustment

--- server.js
+++ server.js
@@ -69,4 +69,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 // Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(process.env.PORT || 3000);
